4996057f16ef82c2bf3e15ea4a53d7e363099208
SudeepMi <sudeepm0599@gmail.com>
2020-08-31 21:19:45 +0545

    Updates

---
+++ precache-manifest.61466dc109eff495046f008431957c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607591d894b4baf3ee8cf840384876ab",
+    "url": "/rock-paper-scissor/index.html"
+  },
+  {
+    "revision": "ab5c03994338c3f35b06",
+    "url": "/rock-paper-scissor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f46653adc9054d525f9",
+    "url": "/rock-paper-scissor/static/js/2.2e0594d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissor/static/js/2.2e0594d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5c03994338c3f35b06",
+    "url": "/rock-paper-scissor/static/js/main.d6dc9eba.chunk.js"
+  },
+  {
+    "revision": "d568c7aa520f67bc1e40",
+    "url": "/rock-paper-scissor/static/js/runtime-main.f0cf6af7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rock-paper-scissor/static/media/logo.5d5d9eef.svg"
+  }
+]);
